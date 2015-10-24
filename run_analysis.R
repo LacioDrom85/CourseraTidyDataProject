687c46ebb41bbd2fd2ba09031eae6364687278bb
LacioDrom85 <francescoingino.85@gmail.com>
2015-10-24 23:17:09 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,85 +1,67 @@
-## From problem description:
-## You should create one R script called run_analysis.R that does the following. 
-##
-## Merges the training and the test sets to create one data set.
-## Extracts only the measurements on the mean and standard deviation for each measurement. 
-## Uses descriptive activity names to name the activities in the data set
-## Appropriately labels the data set with descriptive variable names. 
-## Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
-##
-##From the submission instructions:
-## Please upload your data set as a txt file created with write.table() using row.name=FALSE
-## (do not cut and paste a dataset directly into the text box, as this may cause errors saving your submission).
+require(plyr)
 
-## We begin by putting the script in the same directory where we have the dataset directory
-## (One level up from the actual data files, like features.txt)
+#Utils: function add suffix
+addSuffix<- function(x, suffix) {
+  if (!(x %in% c("Subject","Activity"))) {
+    paste(x,suffix, sep="")
+  }
+  else{
+    x
+  }
+}
 
-## Using R 3.1.1
+#Get data
+pathfile<-file.path(getwd(),"UCI HAR Dataset")
 
-library("reshape2")
+pathfiletest<-file.path(pathfile, "test")
+pathfiletrain<-file.path(pathfile, "train")
+  
+xtest<-read.table(file.path(pathfiletest,"X_test.txt"))
+ytest<-read.table(file.path(pathfiletest,"Y_test.txt"))
+subjecttest<-read.table(file.path(pathfiletest,"subject_test.txt"))
 
-## Step 1-1:
-## READ ALL THE DATA!!
+xtrain<-read.table(file.path(pathfiletrain,"X_train.txt"))
+ytrain<-read.table(file.path(pathfiletrain,"Y_train.txt"))
+subjecttrain<-read.table(file.path(pathfiletrain,"subject_train.txt"))
 
-## basedir <- "UCI HAR Dataset/"
-activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt")
+#Get activity labels 
+activitylabels<-read.table(file.path(pathfile,
+                              			"activity_labels.txt"),
+                            col.names = c("Id", "Activity")
+                            )
 
-features <- read.table("UCI HAR Dataset/features.txt") ## 561 obs of 2 vars, rownums incl
-features <- features[2] ## simplify, cut row numbers
-features <- as.vector(features[,1]) ## it works, but not pretty.
+#Get features labels
+featurelabels<-read.table(file.path(pathfile,
+                            		"features.txt"),
+                            colClasses = c("character")
+                           	)
 
-x_test <- read.table("UCI HAR Dataset/test/X_test.txt", colClasses = c("numeric"), col.names = features) ## 2947 obs of 561 vars
-y_test <- read.table("UCI HAR Dataset/test/y_test.txt") ## 2947 obs of 1 var
-subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt") ## 2947 obs of 1 v
+#1.Merges the training and the test sets to create one data set.
+traindata<-cbind(cbind(xtrain, subjecttrain), ytrain)
+testdata<-cbind(cbind(xtest, subjecttest), ytest)
+sensordata<-rbind(traindata, testdata)
 
-## take only the columns we want
-x_test <- x_test[c(201, 202, 214, 215, 227, 228, 240, 241, 253, 254, 503, 504, 516, 517, 529, 530, 542, 543)]
+sensorlabels<-rbind(rbind(featurelabels, c(562, "Subject")), c(563, "Id"))[,2]
+names(sensordata)<-sensorlabels
 
-x_test[,"activity"] <- y_test
-x_test$activity <- as.factor(x_test$activity)
-x_test[, "subject"] <- subject_test
-x_test$subject <- as.factor(x_test$subject)
+#2. Extracts only the measurements on the mean and standard deviation for each measurement.
+sensordatameanstd <- sensordata[,grepl("mean\\(\\)|std\\(\\)|Subject|Id", names(sensordata))]
 
-## Ugly but it will do
-x_test$activity_labels <- x_test$activity
-x_test$activity_labels <- factor(x_test$activity_labels, levels = c(1,2,3,4,5,6),
-                                 labels = c("WALKING","WALKING_UPSTAIRS",
-                                            "WALKING_DOWNSTAIRS","SITTING",
-                                            "STANDING","LAYING"))
+#3. Uses descriptive activity names to name the activities in the data set
+sensordatameanstd <- join(sensordatameanstd, activitylabels, by = "Id", match = "first")
+sensordatameanstd <- sensordatameanstd[,-1]
 
+#4. Appropriately labels the data set with descriptive names.
+names(sensordatameanstd) <- gsub("([()])","",names(sensordatameanstd))
+#norm names
+names(sensordatameanstd) <- make.names(names(sensordatameanstd))
 
-x_train <- read.table("UCI HAR Dataset/train/X_train.txt", colClasses = c("numeric"), col.names = features) ## 7352 of 561 v
-y_train <- read.table("UCI HAR Dataset/train/y_train.txt") ## 7352 of 1 v
-subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt") ## 7352 of 1 v
- 
-x_train <- x_train[c(201, 202, 214, 215, 227, 228, 240, 241, 253, 254, 503, 504, 516, 517, 529, 530, 542, 543)]
+#5. From the data set in step 4, creates a second, independent tidy data set 
+# with the average of each variable for each activity and each subject 
+finaldata<-ddply(sensordatameanstd, c("Subject","Activity"), numcolwise(mean))
+#improve column names
+finaldataheaders<-names(finaldata)
+finaldataheaders<-sapply(finaldataheaders, addSuffix, ".mean")
+names(finaldata)<-finaldataheaders
 
-x_train[,"activity"] <- y_train
-x_train$activity <- as.factor(x_train$activity)
-x_train[, "subject"] <- subject_train
-x_train$subject <- as.factor(x_train$subject)
-
-x_train$activity_labels <- x_train$activity
-x_train$activity_labels <- factor(x_train$activity_labels, levels = c(1,2,3,4,5,6),
-                                 labels = c("WALKING","WALKING_UPSTAIRS",
-                                            "WALKING_DOWNSTAIRS","SITTING",
-                                            "STANDING","LAYING"))
-
-## Step 1-2:
-## merge all data, free up memory
-
-all_data <- rbind(x_test, x_train)
-# 
-rm(subject_test)
-rm(x_test)
-rm(y_test)
-rm(subject_train)
-rm(x_train)
-rm(y_train)
-rm(features)
-
-#Perform the aggregation on the combined dataset, ignore errors... probably messed something up.
-agg_all <- aggregate(all_data, by= list(all_data$activity_labels, all_data$subject), FUN = mean, na.rm = FALSE)
-
-#write the file out.
-write.table(agg_all, file="tidy_HCI_data.txt", col.names=TRUE)
+write.table(finaldata, file = "sensordata_avg_by_subject.txt", row.name=FALSE)
